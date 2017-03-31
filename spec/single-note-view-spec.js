b1a59f3fcdfcb52ec119e1a61a26b659c47f8438
Eva Balog <evebalog@netscape.net>
2017-03-31 10:29:22 +0100

    Created single note view file and tests

--- spec/single-note-view-spec.js
+++ spec/single-note-view-spec.js
@@ -7,6 +7,10 @@ test({
   },
 
   singleNoteViewreturnsHTMLString: function (){
-
+    var newNote = new Note("I am a note")
+    var singleNoteView = new SingleNoteView(newNote)
+    var returnedString = singleNoteView.returnHTMLString()
+    assert.isEqual("<div>I am a note</div>", returnedString, "singleNoteViewreturnsHTMLString")
+    console.log(singleNoteView);
   }
 })
--- src/single-note-view.js
+++ src/single-note-view.js
@@ -1,13 +1,14 @@
-(function(exports){
+(function(exports) {
 
   function SingleNoteView (Note) {
     this.note = Note
   };
 
-  // SingleNoteView.prototype.returnHTMLString = function () {
-  //   return "<div>" + this.note.showNote + "</div>";
-  //   console.log(returnHTMLString());
-  // };
+  SingleNoteView.prototype.returnHTMLString = function () {
+
+    var noteHTML = "<div>" + this.note._text + "</div>"
+    return noteHTML;
+  };
 
   exports.SingleNoteView = SingleNoteView;
 
